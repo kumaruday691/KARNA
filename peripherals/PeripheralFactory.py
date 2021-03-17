ff41eb699df0c137ab4d7a599a8f79159f771438
Uday kumar Tori <42325680+kumaruday691@users.noreply.github.com>
2021-03-16 23:43:22 -0500

    Update PeripheralFactory.py

--- peripherals/PeripheralFactory.py
+++ peripherals/PeripheralFactory.py
@@ -8,7 +8,7 @@ from peripherals.speaker.SpeechAdapter import SpeechAdapter
 class PeripheralFactory(object):
     # region Constants
 
-    HUE_ADDRESS = '192.168.86.174'
+    HUE_ADDRESS = '192.168.86.28'
 
     # region Properties
 
